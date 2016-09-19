08c98815af50488dd79cc9f1e595681c0e4bbcd4
xvw <xavier.vdw@gmail.com>
2016-09-19 21:32:10 +0200

    Comprehensive example

--- examples/app.ml
+++ examples/app.ml
@@ -3,24 +3,19 @@ open Quasar
 let router app () =
   let open Element in
 
-  let p text =
+  let page app text =
     let x = Html.pcdata text in
-    !![%html "<p>"[x]"</p>"] in
+    (clean app) <+> !![%html "<p>"[x]"</p>"]
+  in
 
-  let i = 99 in 
   
   match [%quasar.routes] with
-  | [%quasar.route "foo"] ->
-    app <+> p "page foo"
-  | [%quasar.route "hello-{string}"] when i > 10 ->
-    app <+> p "page hello"
-  | [%quasar.route "age-{int}-{int}-{bool}"] ->
-    let i, j, k = route_arguments () in
-    app <+> p (Printf.sprintf "page age : %d" i)
-  | "" ->
-    app <+> p "page index"
-  | _  ->
-    app <+> p "unknown page"
+  
+  | [%quasar.route "foo"]                    ->  page app "Page foo"
+  | [%quasar.route "hello-{string}"]         ->  page app "Page Hello"          
+  | [%quasar.route "age-{int}-{int}-{bool}"] ->  page app "Page age"
+  | ""                                       ->  page app "Index du site"
+  | _                                        ->  page app "Page inconnue"
     
     
 
