4d0f3ae4e1d970c01ccf35136dfb0e35e5daeea4
xvw <xavier.vdw@gmail.com>
2016-09-29 16:27:54 +0200

    Append section into slideshow example

--- examples/slideshow-customized/slideshow.ml
+++ examples/slideshow-customized/slideshow.ml
@@ -9,17 +9,32 @@ module Slideshow = S.Simple(struct
     let succ           = S.default_succ
     let pred           = S.default_pred
     let init_slide     = fun ~length ~slides _  -> ()
+
+
+    let perform_section elt slides cursor =
+      let slide = List.nth slides !cursor in
+      match Element.get_data "section" slide with
+      | None -> ()
+      | Some value ->
+        Dom.appendChild
+          (Element.clean elt)
+          (Element.text value)
                           
     
     let update ~length ~slides cursor =
       let open Element in
       let _ = S.default_update ~length ~slides cursor in
-      match getById_opt "footer" with
+      let () =
+        match getById_opt "pagination" with
+        | None -> ()
+        | Some elt ->
+          Dom.appendChild
+            (clean elt)
+            (text $ Printf.sprintf "%d/%d" (!cursor+1) length) in
+      match getById_opt "section" with
       | None -> ()
-      | Some elt ->
-        Dom.appendChild
-          (clean elt)
-          (text $ Printf.sprintf "%d/%d" (!cursor+1) length)
+      | Some elt -> perform_section elt slides cursor
+        
 
     let before ~length ~slides cursor =
       S.default_before ~length ~slides cursor update
