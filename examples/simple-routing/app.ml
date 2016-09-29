b441c1abde8a77d022f4d0a3863703dc5b63f665
xvw <xavier.vdw@gmail.com>
2016-09-29 17:04:59 +0200

    Append simple routing example

---
+++ examples/simple-routing/app.ml
@@ -0,0 +1,45 @@
+open Quasar
+
+let simple_h1 txt =
+  let x = Html.pcdata txt in
+  !![%html "<h1>"[x]"</h1>"]
+
+let index parent txt =
+  let _ = Element.(clean parent <+> simple_h1 txt)
+  in ()
+
+
+let hello parent name =
+  index parent ("Hello "^name)
+
+
+let age parent a name =
+  let str = string_of_int a in
+  index parent ("Hello "^name^", you are "^str^" old !")
+
+
+let error404 parent =
+  index parent "404 Error"
+  
+                      
+
+let routes parent () =
+  match [%routes] with
+  | "" -> index parent "Hello World"
+            
+  | [%route "hello-{string}"] ->
+    let name = route_arguments () in
+    hello parent name
+
+  | [%route "age-{int}-name-{string}"] ->
+    let a, name = route_arguments () in
+    age parent a name
+  
+  | _  -> error404 parent
+
+
+let () = start (fun () ->
+    match Element.getById_opt "app" with
+    | None        -> alert "Unable to boot the application"
+    | Some parent -> Router.start (routes parent) 
+  )
