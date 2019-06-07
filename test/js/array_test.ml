d45dbb7e9d352dc2c7d72abb6424e1827d9da296
xvw <xaviervdw@gmail.com>
2019-06-07 17:31:11 +0200

    Add some test (and use id instead of fun x -> x)

--- test/js/array_test.ml
+++ test/js/array_test.ml
@@ -15,14 +15,14 @@ let empty_array () =
 let init1 () =
   test "Create an array using init - 1" (fun () ->
       let expected : int J.Array.t = arr "[0, 1, 2, 3, 4, 5]" in
-      let input = J.Array.init 6 (fun x -> x) in
+      let input = J.Array.init 6 id in
       (expect input).%{to_strict_equal} <- expected)
 ;;
 
 let init2 () =
   test "Create an array using init - 2" (fun () ->
       let expected : int J.Array.t = arr "[]" in
-      let input = J.Array.init 0 (fun x -> x) in
+      let input = J.Array.init 0 id in
       (expect input).%{to_strict_equal} <- expected)
 ;;
 
@@ -40,8 +40,105 @@ let prefilled2 () =
       (expect input).%{to_strict_equal} <- expected)
 ;;
 
+let from_array1 () =
+  test "Create an array from an OCaml array - 1" (fun () ->
+      let expected : int J.Array.t = arr "[]" in
+      let input = J.Array.from_array id [||] in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let from_array2 () =
+  test "Create an array from an OCaml array - 2" (fun () ->
+      let expected : int J.Array.t = arr "[1, 2, 3, 4, 5]" in
+      let input = J.Array.from_array id [| 1; 2; 3; 4; 5 |] in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let from_array3 () =
+  test "Create an array from an OCaml array - 3" (fun () ->
+      let expected : Js.js_string Js.t J.Array.t =
+        arr "['1', '2', '3', '4', '5']"
+      in
+      let input =
+        J.Array.from_array
+          (fun x -> string_of_int x |> Js.string)
+          [| 1; 2; 3; 4; 5 |]
+      in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let from_list1 () =
+  test "Create an array from an OCaml list - 1" (fun () ->
+      let expected : int J.Array.t = arr "[]" in
+      let input = J.Array.from_list id [] in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let from_list2 () =
+  test "Create an array from an OCaml list - 2" (fun () ->
+      let expected : int J.Array.t = arr "[1, 2, 3, 4, 5]" in
+      let input = J.Array.from_list id [ 1; 2; 3; 4; 5 ] in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let from_list3 () =
+  test "Create an array from an OCaml list - 3" (fun () ->
+      let expected : Js.js_string Js.t J.Array.t =
+        arr "['1', '2', '3', '4', '5']"
+      in
+      let input =
+        J.Array.from_list
+          (fun x -> string_of_int x |> Js.string)
+          [ 1; 2; 3; 4; 5 ]
+      in
+      (expect input).%{to_strict_equal} <- expected)
+;;
+
+let length1 () =
+  test "Test the length of an array - 1" (fun () ->
+      let array = J.Array.empty () in
+      let input = J.Array.length array in
+      (expect input).%{to_be} <- 0)
+;;
+
+let length2 () =
+  test "Test the length of an array - 2" (fun () ->
+      let array = J.Array.from_list id [ 1; 2; 3 ] in
+      let input = J.Array.length array in
+      (expect input).%{to_be} <- 3)
+;;
+
+let get1 () =
+  test "Test get - 1" (fun () ->
+      let array = J.Array.empty () in
+      let input = J.Array.get array 2 |> Js.Opt.option in
+      (expect input).%{to_be_null})
+;;
+
+let get2 () =
+  test "Test get - 2" (fun () ->
+      let array = J.Array.from_list id [ 1; 2; 3 ] in
+      let input = J.Array.get array 2 |> Js.Opt.option in
+      (expect input).%{to_be} <- (Js.Opt.return 3))
+;;
+
 let suite () =
   List.iter
     (fun t -> t ())
-    [ empty_array; init1; init2; prefilled1; prefilled2 ]
+    [ empty_array
+    ; init1
+    ; init2
+    ; prefilled1
+    ; prefilled2
+    ; from_array1
+    ; from_array2
+    ; from_array3
+    ; from_list1
+    ; from_list2
+    ; from_list3
+    ; length1
+    ; length2
+    ; get1
+    ; get2
+    ]
 ;;
